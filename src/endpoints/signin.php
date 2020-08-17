c96fc24b49a69825c36524e66a69c0d8c57cebb2
ling1729 <renlingfeng21@gmail.com>
2020-08-16 19:27:53 -0700

    test

--- src/endpoints/signin.php
+++ src/endpoints/signin.php
@@ -11,14 +11,14 @@ if (isset($_REQUEST['password'])) {
   $sessionTime = $currentTime - $lastTime;
   if($userData[2] == "FALSE"){
     $values = [
-      ["TRUE", $currentTime, $totalTime]
+      ["TRUE", strval($currentTime), strval($totalTime)]
     ];
     $range = ("C" . getUserRow($_REQUEST['password'])) . (":E" . getUserRow($_REQUEST['password']));
     changeData($values, $range);
     echo $userData[0];
   } else if($userData[2] == "TRUE"){
     $values = [
-      ["FALSE", $currentTime, $sessionTime]
+      ["FALSE", strval($currentTime), strval($sessionTime)]
     ];
     $range = ("C" . getUserRow($_REQUEST['password'])) . (":E" . getUserRow($_REQUEST['password']));
     echo json_encode($userData);
