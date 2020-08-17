55d7c03a842d06eb5d0478bca28e449a0a79176e
ling1729 <renlingfeng21@gmail.com>
2020-08-16 19:14:50 -0700

    I swear if this works

--- src/endpoints/signin.php
+++ src/endpoints/signin.php
@@ -7,19 +7,20 @@ if (isset($_REQUEST['password'])) {
   $userData = getUserData($_REQUEST['password']);
   $lastTime = (int) userData[3];
   $totalTime = (int) userData[4];
+  $sessionTime = time() - $lastTime;
   if($userData[2] == "FALSE"){
     $values = [
-      ["TRUE", time(), (int) userData[4]]
+      ["TRUE", $currentTime, (int) userData[4]]
     ];
     $range = ("C" . getUserRow($_REQUEST['password'])) . (":E" . getUserRow($_REQUEST['password']));
     changeData($values, $range);
     echo $userData[0];
   } else if($userData[2] == "TRUE"){
     $values = [
-      ["FALSE", time(), (int) userData[4] + time() - (int) userData[3]]
+      ["FALSE", $currentTime, (int) userData[4] + time() - (int) userData[3]]
     ];
     $range = ("C" . getUserRow($_REQUEST['password'])) . (":E" . getUserRow($_REQUEST['password']));
-    echo (time() - $lastTime);
+    echo $sessionTime;
     changeData($values, $range);
     echo $userData[0];
   }
