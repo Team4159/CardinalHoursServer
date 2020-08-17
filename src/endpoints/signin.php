687fa85fe4a1af736ae777b645ddd2dbde900a8f
ling1729 <renlingfeng21@gmail.com>
2020-08-16 19:02:56 -0700

    whyyyyy

--- src/endpoints/signin.php
+++ src/endpoints/signin.php
@@ -19,7 +19,7 @@ if (isset($_REQUEST['password'])) {
       ["FALSE", time(), (int) userData[4] + time() - (int) userData[3]]
     ];
     $range = ("C" . getUserRow($_REQUEST['password'])) . (":E" . getUserRow($_REQUEST['password']));
-    echo time()-$lastTime;
+    echo time();
     changeData($values, $range);
     echo $userData[0];
   }
