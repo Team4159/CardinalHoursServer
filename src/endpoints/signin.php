19df5d61c8ddaf191e4d394f2e9e3aef18a1e49f
ling1729 <renlingfeng21@gmail.com>
2020-08-16 20:24:03 -0700

    implement max hours

--- src/endpoints/signin.php
+++ src/endpoints/signin.php
@@ -2,31 +2,28 @@
 require('sheets.php');
 require('datafuncs.php');
 
+$MAX_TIME = 43200 // 12 hours
+
 // start tracking time on signin
 if (isset($_REQUEST['password'])) {
   $userData = getUserData($_REQUEST['password']);
   $lastTime = (int) $userData[3];
   $totalTime = (int) $userData[4];
   $sessionTime = time() - $lastTime;
-  echo $sessionTime;
 
   if($userData[2] == "FALSE"){
     $values = [
       ["TRUE", time(), $totalTime]
     ];
     $range = ("C" . getUserRow($_REQUEST['password'])) . (":E" . getUserRow($_REQUEST['password']));
-
     changeData($values, $range);
-
     echo $userData[0];
   } else if($userData[2] == "TRUE"){
     $values = [
-      ["FALSE", time(), $totalTime + $sessionTime]
+      ["FALSE", time(), $totalTime + $sessionTime < $MAX_TIME ? $sessionTime : 0]
     ];
     $range = ("C" . getUserRow($_REQUEST['password'])) . (":E" . getUserRow($_REQUEST['password']));
-
     changeData($values, $range);
-
     echo $userData[0];
   }
 }
