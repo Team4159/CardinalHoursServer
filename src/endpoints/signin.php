f61755672440bb719e62438bfea4d0fe3bc4bf49
ling1729 <renlingfeng21@gmail.com>
2020-08-16 17:38:10 -0700

    fied the problem yay

--- src/endpoints/signin.php
+++ src/endpoints/signin.php
@@ -4,7 +4,6 @@ require('datafuncs.php');
 
 // start tracking time on signin
 if (isset($_REQUEST['password'])) {
-  echo getUserData($_REQUEST['password'])[2];
   if(getUserData($_REQUEST['password'])[2] == FALSE){
     echo getUserData($_REQUEST['password'])[0];
   }
