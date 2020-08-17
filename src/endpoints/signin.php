716fe43f5c6b0b8638a538805ab2e2fb28bc9baf
ling1729 <renlingfeng21@gmail.com>
2020-08-16 17:36:30 -0700

    stuff

--- src/endpoints/signin.php
+++ src/endpoints/signin.php
@@ -4,6 +4,7 @@ require('datafuncs.php');
 
 // start tracking time on signin
 if (isset($_REQUEST['password'])) {
+  echo getUserData($_REQUEST['password'])[3];
   if(getUserData($_REQUEST['password'])[3] == FALSE){
     echo getUserData($_REQUEST['password'])[0];
   }
