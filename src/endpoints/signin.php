5d6b1b89bc39a00874519abf307745da72734780
ling1729 <renlingfeng21@gmail.com>
2020-08-16 18:51:40 -0700

    figure out why time is wonked

--- src/endpoints/signin.php
+++ src/endpoints/signin.php
@@ -17,6 +17,7 @@ if (isset($_REQUEST['password'])) {
       ["FALSE", time(), (int) userData[4] + time() - (int) userData[3]]
     ];
     $range = ("C" . getUserRow($_REQUEST['password'])) . (":E" . getUserRow($_REQUEST['password']));
+    echo time() - ((int) userData[3]);
     changeData($values, $range);
     echo $userData[0];
   }
