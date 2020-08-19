db8032f034794c1ec14731695e1ecab81bfad8d9
Kai Chang <kaijchang@gmail.com>
2020-08-19 12:34:14 -0700

    allow all origins

--- src/endpoints/cors.php
+++ src/endpoints/cors.php
@@ -1,13 +1,9 @@
 <?php
 function cors() {
     // Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-        // you want to allow, and if so:
-        header("Access-Control-Allow-Origin: *");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
 
     // Access-Control headers are received during OPTIONS requests
     if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
