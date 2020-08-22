062e79e1cc0b7cc47b3ca45616e62fa4aa4b7b01
ling1729 <renlingfeng21@gmail.com>
2020-08-22 16:14:13 -0700

    seperate sign in and out

--- src/endpoints/signin.php
+++ src/endpoints/signin.php
@@ -1,42 +1,12 @@
 <?php
 require('aws.php');
 require('cors.php');
-$MAX_TIME = 43200; // 12 hours
 
-# cors();
+cors();
 
-function signIn($password, $did = ''){
-  global $MAX_TIME;
+function signIn($password){
   $userData = getUser($password);
-  $lastTime = $userData["lastTime"]["N"];
-  $totalTime = $userData["totalTime"]["N"];
-  $sessionTime = time() - $lastTime;
-  if($userData["signedIn"]["BOOL"]){
-    $data = [
-      ':signedIn' => ['BOOL' => false],
-      ':lastTime' => ['N' => strval(time())],
-      ':totalTime' => ['N' => strval($totalTime + ($sessionTime < $MAX_TIME ? $sessionTime : 0))]
-    ];
-
-    $session = [
-      ':empty_list' => ['L'=>[]],
-      ':session' => [
-        'L' => [
-          [
-            'M' => [
-              'date' => ['S' => date("m.d.Y")],
-              'time' => ['N' => strval($sessionTime)],
-              'did' => ['S' => $did]
-            ]
-          ]
-        ]
-      ]
-    ];
-
-    updateUser($password, $data);
-    addSession($password, $session);
-    echo $userData["username"]["S"];
-  } else {
+  if(!$userData["signedIn"]["BOOL"]){
     $data = [
       ':signedIn' => ['BOOL' => true],
       ':lastTime' => ['N' => strval(time())],
@@ -48,9 +18,6 @@ function signIn($password, $did = ''){
 }
 // start tracking time on signin
 if (isset($_REQUEST['password'])) {
-  if(isset($_REQUEST['did']))
-    signIn($_REQUEST['password'], $_REQUEST['did']);
-  else
     signIn($_REQUEST['password']);
 }
 ?>
---
+++ src/endpoints/signout.php
@@ -0,0 +1,49 @@
+<?php
+require('aws.php');
+require('cors.php');
+$MAX_TIME = 43200; // 12 hours
+
+cors();
+
+function signOut($password, $did = ''){
+  global $MAX_TIME;
+  $userData = getUser($password);
+  $lastTime = $userData["lastTime"]["N"];
+  $totalTime = $userData["totalTime"]["N"];
+  $sessionTime = time() - $lastTime;
+  if($userData["signedIn"]["BOOL"]){
+    $data = [
+      ':signedIn' => ['BOOL' => false],
+      ':lastTime' => ['N' => strval(time())],
+      ':totalTime' => ['N' => strval($totalTime + ($sessionTime < $MAX_TIME ? $sessionTime : 0))]
+    ];
+
+    $session = [
+      ':empty_list' => ['L'=>[]],
+      ':session' => [
+        'L' => [
+          [
+            'M' => [
+              'date' => ['S' => date("m.d.Y")],
+              'time' => ['N' => strval($sessionTime)],
+              'did' => ['S' => $did]
+            ]
+          ]
+        ]
+      ]
+    ];
+
+    updateUser($password, $data);
+    addSession($password, $session);
+    echo $userData["username"]["S"];
+  }
+}
+
+// start tracking time on signin
+if (isset($_REQUEST['password'])) {
+  if(isset($_REQUEST['did']))
+    signOut($_REQUEST['password'], $_REQUEST['did']);
+  else
+    signOut($_REQUEST['password']);
+}
+?>
