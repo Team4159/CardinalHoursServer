1cb5616dd17c552bbace6b1a6bf5c94dfe9f6c2c
ling1729 <renlingfeng21@gmail.com>
2020-08-22 12:53:38 -0700

    fix getdata

--- src/endpoints/getuserdata.php
+++ src/endpoints/getuserdata.php
@@ -2,5 +2,7 @@
 require('aws.php');
 require('cors.php');
 cors();
-echo json_encode(getUser($_REQUEST['password']));
+use Aws\DynamoDb\Marshaler;
+$marshaler = new Marshaler();
+echo $marshaler->marshalItem(getUser($_REQUEST['password']));
 ?>
