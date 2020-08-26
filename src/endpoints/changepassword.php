fa6a314f9c4a99c70451d7f0259848172f2df5b7
ling1729 <renlingfeng21@gmail.com>
2020-08-26 11:16:53 -0700

    fix stuff

--- src/endpoints/changepassword.php
+++ src/endpoints/changepassword.php
@@ -5,9 +5,9 @@ require('cors.php');
 cors();
 
 if (isset($_REQUEST['newPassword']) && isset($_REQUEST['password'])) {
-  if(getUser($_REQUEST['newPassword']) === null && getUser($_REQUEST['password']) && $_REQUEST['newPassword'] != '' && $_REQUEST['password'] != ''){
+  if(getUser($_REQUEST['newPassword']) === null && getUser($_REQUEST['password']) != null && $_REQUEST['newPassword'] != '' && $_REQUEST['password'] != ''){
     changePassword($_REQUEST['username'], $_REQUEST['newPassword']);
-    echo $_REQUEST['username'];
+    echo $_REQUEST['newPassword'];
   } else
     http_response_code(404);
 }
