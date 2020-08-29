3acd5a1688e51e706495bddc25519ec1890efb33
Kai Chang <kaijchang@gmail.com>
2020-08-29 16:16:48 -0700

    don't allow dupe usernames'

--- src/endpoints/adduser.php
+++ src/endpoints/adduser.php
@@ -5,7 +5,8 @@ require('cors.php');
 cors();
 
 if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
-  if(getUser(['password' => ['S' => strval($_REQUEST['password'])]]) === null && $_REQUEST['username'] != '' && $_REQUEST['password'] != ''){
+  if(getUser(['password' => ['S' => strval($_REQUEST['password'])]]) === null &&
+    getUser(['username' => ['S' => strval($_REQUEST['username'])]]) === null && $_REQUEST['username'] != '' && $_REQUEST['password'] != ''){
     addUser($_REQUEST['username'], $_REQUEST['password']);
     echo $_REQUEST['username'];
   } else
