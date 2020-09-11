c12a4c6df0ae11f51426e6ba868aab3c14c2cce0
ling1729 <renlingfeng21@gmail.com>
2020-09-11 16:12:00 -0700

    fix stuff

--- src/endpoints/getpassword.php
+++ src/endpoints/getpassword.php
@@ -5,12 +5,10 @@ cors();
 use Aws\DynamoDb\Marshaler;
 $marshaler = new Marshaler();
 if(isset($_REQUEST['name'])){
-  $first = explode(' ', $_REQUEST['name'], 2)[0];
-  $last = explode(' ', $_REQUEST['name'], 2)[1];
-  if(getUserPassword($first, $last) === null)
+  if(getUserPassword($_REQUEST['name']) === null)
     http_response_code(404);
   else {
-    echo getUserPassword($first, $last);
+    echo getUserPassword($_REQUEST['name']);
   }
 }
 ?>
