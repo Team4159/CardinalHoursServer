12b06fb571263e30d0ce5f31762e8c725755752b
Kai Chang <kaijchang@gmail.com>
2020-09-19 19:44:44 -0700

    allow correcting session time

--- src/endpoints/signout.php
+++ src/endpoints/signout.php
@@ -1,24 +1,28 @@
 <?php
+require('aws.php');
 require('cors.php');
-require('syncsheets.php');
 $MAX_TIME = 43200; // 12 hours
-$MIN_TIME = 1;  // 1 second
 
 cors();
 
-function signOut($password, $did = ''){
+function signOut($password, $did = '', $sessionTime){
   global $MAX_TIME;
-  global $MIN_TIME;
-
   $userData = getUser(['password' => ['S' => $password]]);
   $lastTime = $userData["lastTime"]["N"];
   $totalTime = $userData["totalTime"]["N"];
-  $sessionTime = time() - $lastTime;
+  $now = time();
+  if ($sessionTime && is_numeric($sessionTime)) {
+    $sessionTime = intval($sessionTime);
+    $date = $lastTime + $sessionTime;
+  } else {
+    $sessionTime = $now - $lastTime;
+    $date = $now;
+  }
   if($userData["signedIn"]["BOOL"]){
     $data = [
       ':signedIn' => ['BOOL' => false],
       ':lastTime' => ['N' => strval(time())],
-      ':totalTime' => ['N' => strval($totalTime + ($sessionTime <= $MAX_TIME ? $sessionTime : 0))]
+      ':totalTime' => ['N' => strval($totalTime + ($sessionTime < $MAX_TIME ? $sessionTime : 0))]
     ];
 
     $session = [
@@ -27,7 +31,7 @@ function signOut($password, $did = ''){
         'L' => [
           [
             'M' => [
-              'date' => ['S' => strval(time())],
+              'date' => ['S' => strval($date)],
               'time' => ['N' => strval($sessionTime)],
               'did' => ['S' => strval($did)],
               'day' => ['S' => date("m/d/Y")],
@@ -37,12 +41,10 @@ function signOut($password, $did = ''){
         ]
       ]
     ];
-    if($sessionTime > $MIN_TIME){
-      updateUser($password, $data);
-      addSession($password, $session);
-      echo $userData["username"]["S"];
-      syncUser($password);
-    }
+
+    updateUser($password, $data);
+    addSession($password, $session);
+    echo $userData["username"]["S"];
   }
 }
 
@@ -51,10 +53,7 @@ if (isset($_REQUEST['password'])) {
   if(getUser(['password' => ['S' => strval($_REQUEST['password'])]]) === null){
     http_response_code(404);
   } else {
-    if(isset($_REQUEST['did']))
-      signOut($_REQUEST['password'], $_REQUEST['did']);
-    else
-      signOut($_REQUEST['password']);
+    signOut($_REQUEST['password'], $_REQUEST['did'], $_REQUEST['sessionTime']);
   }
 }
 ?>
