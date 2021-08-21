85bde77a7d9928f52847ba4015a79d3d63ac2d5b
ling1729 <renlingfeng21@gmail.com>
2021-08-20 17:01:45 -0700

    start using sql

--- routes/users.ts
+++ routes/users.ts
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+import Database from 'better-sqlite3';
+const db = new Database('foobar.db', { verbose: console.log });
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/add', function(req, res, next) {
+  const createTable = db.prepare("CREATE TABLE IF NOT EXISTS cats ('id' INT, 'name' VARCHAR, 'age' INT);");
+  const insert = db.prepare('INSERT INTO cats (name, age) VALUES (@name, @age)');
+  createTable.run();
+
 });
 
 module.exports = router;
