2ec4956c525ebc8580234b06bddf073bf31e89c7
ling1729 <renlingfeng21@gmail.com>
2021-08-30 17:31:23 -0700

    create tables with mysql

--- routes/users.ts
+++ routes/users.ts
@@ -3,22 +3,29 @@ import mysql from 'mysql';
 
 require('dotenv').config();
 const router = express.Router();
-const con = mysql.createConnection({
+
+const con = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
 });
 
-con.connect();
+con.getConnection(function(err, connection) {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+})
 
-const createTables = `
+const createUserTable = `
   CREATE TABLE IF NOT EXISTS users(
     name TEXT NOT NULL,
-    password TEXT NOT NULL PRIMARY KEY,
+    password TEXT NOT NULL,
     signedIn BOOL DEFAULT false,
     lastTime INT NOT NULL
-  );
+  )
+`;
+
+const createSessionTable = `
   CREATE TABLE IF NOT EXISTS sessions(
     password TEXT NOT NULL,
     startTime INT NOT NULL,
@@ -26,7 +33,21 @@ const createTables = `
   )
 `;
 
-db.exec(createTables);
+con.query(createUserTable, function (err, res) {
+  if (err) throw err;
+  if(res.warningCount !== 0)
+    console.log("User table already exists");
+  else
+    console.log("Created new user table");
+});
+
+con.query(createSessionTable, function (err, res) {
+  if (err) throw err;
+  if(res.warningCount !== 0)
+    console.log("Session table already exists");
+  else
+    console.log("Created new session table");
+});
 
 /* GET users listing. */
 router.get('/', (req, res) => {
@@ -40,28 +61,42 @@ router.post('/adduser', (req, res) => {
       message: "Username and password cannot be empty"
     }); 
   } else {
-    const stmnt = db.prepare("INSERT INTO users(name, password, signedIn, lastTime) VALUES(?, ?, ?, ?)")
-    stmnt.run(username, password, 0, Date.now());
+    const addUser = "INSERT INTO users(name, password, signedIn, lastTime) VALUES(?, ?, ?, ?)";
+    con.query(mysql.format(addUser, [username, password, 0, Date.now()]), function (err, res) {
+      if (err) throw err;
+      return res;
+    });
     res.send({msg: `added new user:${username}`});
   }
 });
 
 router.post('/signin', (req, res) => {
-  const getUser = db.prepare(`SELECT name, signedIn FROM users WHERE password = ?`);
-  const signIn = db.prepare("UPDATE users SET lastTime = ?, signedIn = 1 WHERE password = ?");
-  let user: boolean = getUser.get(req.body.password);
+  const getUser = "SELECT name, signedIn FROM users WHERE password = ?";
+  const signIn = "UPDATE users SET lastTime = ?, signedIn = 1 WHERE password = ?";
+
+  var user;
+  con.query(mysql.format(getUser, [req.body.password]), function (err, res) {
+    if (err) throw err;
+    user = res;
+  });
+
+  return user;
   if( user['signedIn'] ){
     res.status(400).send({
       message: "User already signed in"
     });
   } else {
-    signIn.run(Date.now(), req.body.password);
+    con.query(mysql.format(signIn, [req.body.password]), function (err, res) {
+      if (err) return err;
+      user = res;
+    });
     res.status(200).send({
       msg: `signed in user: ${user['name']}`
     });
   }
 });
 
+/*
 router.post('/signout', (req, res) => {
   const getUser = db.prepare("SELECT signedIn, lastTime FROM users WHERE password = ?");
   const signOut = db.prepare("UPDATE users SET signedIn = 0 WHERE password = ?");
@@ -79,7 +114,9 @@ router.post('/signout', (req, res) => {
     });
   }
 });
+*/
 
+/*
 router.get('/all', (req, res) => {
   const stmnt = db.prepare("SELECT * FROM users")
   const users = stmnt.all()
@@ -114,5 +151,5 @@ const isDuplicateUsername = (username) => {
     if(username === usernames[i]) return true
   return false
 }
-
+*/
 module.exports = router;
