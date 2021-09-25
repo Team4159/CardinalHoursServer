4f62aadd4fdbb85b97d8250c8021eadd525efbc2
ling1729 <renlingfeng21@gmail.com>
2021-09-25 02:06:52 -0700

    cache all get requests with redis

--- db.ts
+++ db.ts
@@ -1,16 +1,30 @@
-import mysql from 'mysql-await';
+import mysql from 'mysql2';
+const asyncRedis = require("async-redis");
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const { MysqlRedisAsync, HashTypes, Caching } = require("mysql-redis");
+
+const cacheOptions = {
+    expire: 2629746,// seconds, defaults to 30 days
+    keyPrefix: "sql.", // default
+    hashType: HashTypes.md5, //default
+    caching: Caching.CACHE //default
+};
+
+const redis = asyncRedis.createClient();
+
+const poolPromise = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
-});
+}).promise();
 
-pool.getConnection(function(err, conn) {
-  if (err) throw err;
-  console.log('connected as id ' + conn.threadId);
-})
+const mysqlRedis = new MysqlRedisAsync(
+    poolPromise,
+    redis
+);
 
-exports.db = pool;
+exports.Caching = Caching;
+exports.HashTypes = HashTypes;
+exports.db = mysqlRedis;
--- routes/users.ts
+++ routes/users.ts
@@ -1,7 +1,9 @@
-import mysql from 'mysql-await';
+import mysql from 'mysql2';
 import Router from 'express-promise-router';
 var database = require('../db');
 const router = Router();
+
+const {Caching, HashTypes} = require('../db');
 
 const db = database.db;
 
@@ -23,6 +25,11 @@ const createSessionTable: string = `
     endTime BIGINT NOT NULL
   )
 `;
+
+const getUser = "SELECT id, name, signedIn, lastTime FROM users WHERE password = BINARY ?";
+const getUserSessions = "SELECT id, startTime, endTime FROM sessions WHERE password = BINARY ?";
+const getUsers = "SELECT id, name, password, signedIn, lastTime FROM users";
+const getSessions = "SELECT password, startTime, endTime FROM sessions";
 
 db.query(createUserTable, function (err, res) {
   if (err) throw err;
@@ -46,8 +53,6 @@ router.get('/', (req, res) => {
 });
 
 router.post('/adduser', async (req, res, next) => {
-  const con = await db.awaitGetConnection();
-  await con.awaitBeginTransaction();
   const { username, password }: { username: string, password: string } = req.body;
 
   if( username === undefined || password === undefined || username == "" || password == "" ){
@@ -56,208 +61,220 @@ router.post('/adduser', async (req, res, next) => {
   }
 
   const addUser: string = "INSERT INTO users(name, password, signedIn, lastTime) VALUES(?, ?, ?, ?)";
-  con.query(mysql.format(addUser, [username, password, 0, Date.now()]), function (error, response) {
-    if (error) {
+  db.query(mysql.format(addUser, [username, password, 0, Date.now()]), {caching: Caching.SKIP})
+    .then(error => {
+      refreshCache();
+      console.log(`Added new user: ${username}, ${password}`);
+      res.status(200).send(`Added new user: ${username}, password: ${password}`);
+      return;
+    })
+    .catch(error => {
       if(error.errno === 1062){
-        con.awaitRollback();
         res.status(400).send('User already exists');
         return;
       }
       else {
-        con.awaitRollback();
         console.log(error);
         res.status(500).send('Something went wrong');
         return;
       }
-    } else {
-      con.awaitCommit();
-      console.log(`Added new user: ${username}, ${password}`);
-      res.status(200).send(`Added new user: ${username}, password: ${password}`);
-    }
-  });
-
-  con.release();
+    });
 });
 
 router.post('/signin', async (req, res, next) => {
-  const con = await db.awaitGetConnection();
-  await con.awaitBeginTransaction();
-  const getUser = "SELECT name, signedIn FROM users WHERE password = BINARY ?";
   const signIn = "UPDATE users SET lastTime = ?, signedIn = 1 WHERE password = BINARY ?";
 
-  var user = await db.awaitQuery(mysql.format(getUser, [req.body.password]));
+  var user = (await db.query(mysql.format(getUser, [req.body.password]), {hash: "getUser"}))[0];
   if( user.length === 0 ){
-    con.awaitRollback();
     res.status(404).send(`User not found`);
     return;
   }
 
   if( user[0]['signedIn'] === 1 ){
-    con.awaitRollback();
     res.status(400).send("User already signed in");
     return;
   }
 
-  con.query(mysql.format(signIn, [Date.now(), req.body.password]), function (error, response) {
-    if (error) {
-      con.awaitRollback();
+  db.query(mysql.format(signIn, [Date.now(), req.body.password]), {caching: Caching.SKIP})
+    .then(response => {
+      refreshUserCache(req.body.password);
+      res.status(200).send(`Signed in user: ${user[0]['name']}`);
+      return;
+    })
+    .catch(error => {
       console.log(error);
       res.status(500).send('Something went wrong');
       return;
-    } else {
-      con.awaitCommit();
-      res.status(200).send(`Signed in user: ${user[0]['name']}`);
-    }
-  });
-
-  con.release();
+    });
 });
 
 router.post('/addsession', async (req, res, next) => {
-  const con = await db.awaitGetConnection();
-  await con.awaitBeginTransaction();
-  const getUser = "SELECT name, signedIn, lastTime FROM users WHERE password = BINARY ?";
+  const con = await db.getConnection();
   const addSession = "INSERT INTO sessions(password, startTime, endTime) VALUES(?, ?, ?)";
 
-  var user = await db.awaitQuery(mysql.format(getUser, [req.body.password]));
+  var user = (await db.query(mysql.format(getUser, [req.query.password]), {hash: "getUser"}))[0];
   if( user.length === 0 ){
-    con.awaitRollback();
     res.status(404).send(`User not found`);
     return;
   }
 
-  con.query(mysql.format(addSession, [req.body.password, req.body.startTime, req.body.endTime]), function (error, response) {
-    if (error) {
-      con.awaitRollback();
+  con.query(mysql.format(addSession, [req.body.password, req.body.startTime, req.body.endTime]), {caching: Caching.SKIP})
+    .then(response => {
+      refreshUserCache(req.body.password);
+      refreshCache();
+      res.status(200).send(`Added session for user: ${user[0]['name']}`);
+      return;
+    })
+    .catch(error => {
       console.log(error);
       res.status(500).send('Something went wrong');
       return;
-    } else {
-      con.awaitCommit();
-      res.status(200).send(`Added session for user: ${user[0]['name']}`);
-    }
-  });
-  con.release();
+    });
 });
 
 router.post('/signout', async (req, res, next) => {
-  const con = await db.awaitGetConnection();
-  await con.awaitBeginTransaction();
-  const getUser = "SELECT name, signedIn, lastTime FROM users WHERE password = BINARY ?";
   const signOut = "UPDATE users SET signedIn = 0 WHERE password = BINARY ?";
   const addSession = "INSERT INTO sessions(password, startTime, endTime) VALUES(?, ?, ?)";
 
-  var user = await db.awaitQuery(mysql.format(getUser, [req.body.password]));
+  var user = (await db.query(mysql.format(getUser, [req.body.password]), {hash: "getUser"}))[0];
   if( user.length === 0 ){
-    con.awaitRollback();
     res.status(404).send(`User not found`);
     return;
   }
 
   if( user[0]['signedIn'] === 0 ){
-    con.awaitRollback();
     res.status(400).send("User not signed in");
     return;
   }
 
-  con.query(mysql.format(signOut, [req.body.password]), function (error, response) {
-    if (error) {
-      con.awaitRollback();
+  db.query(mysql.format(signOut, [req.body.password]), {caching: Caching.SKIP})
+    .then(response => {
+      refreshUserCache(req.body.password);
+      return;
+    })
+    .catch(error => {
       console.log(error);
       res.status(500).send('Something went wrong');
       return;
-    }
-  });
+    });
 
-  con.query(mysql.format(addSession, [req.body.password, user[0]['lastTime'], Date.now()]), function (error, response) {
-    if (error) {
-      con.awaitRollback();
-      console.log(error);
-      res.status(500).send('Something went wrong');
-      return;
-    } else {
-      con.awaitCommit();
+  db.query(mysql.format(addSession, [req.body.password, user[0]['lastTime'], Date.now()]), {caching: Caching.SKIP})
+    .then(response => {
+      refreshCache();
       res.status(200).send(`Signed out user: ${user[0]['name']}`);
-    }
+      return;
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).send('Something went wrong');
+      return;
   });
-
-  con.release();
 });
 
+function refreshUserCache(password){
+  db.query(mysql.format(getUser, [password]), {hash: "getUser", caching: Caching.REFRESH})
+    .catch(error => {
+      console.log(error);
+      return;
+    });
+
+  db.query(mysql.format(getUserSessions, [password]), {hash: "getUserSessions", caching: Caching.REFRESH})
+    .catch(error => {
+      console.log(error);
+      return;
+    });
+}
+
+function refreshCache(){
+  db.query(getUsers, {hash: "getUsers", caching: Caching.REFRESH})
+    .catch(error => {
+      console.log(error);
+      return;
+    });
+
+  db.query(getSessions, {hash: "getSessions", caching: Caching.REFRESH})
+    .catch(error => {
+      console.log(error);
+      return;
+    });
+}
+
 router.post('/changeSessionTime', async (req, res, next) => {
-  
+
+});
+
+router.get('/test', async (req, res, next) => {
+  db.query(mysql.format(getUser, [req.query.password]), {caching: Caching.REFRESH})
+    .then(response => {
+      res.json(response);
+    })
 });
 
 router.get('/getusersessions', async (req, res, next) => {
-  const getUser = "SELECT name FROM users WHERE password = BINARY ?";
-
-  var user = await db.awaitQuery(mysql.format(getUser, [req.query.password]));
+  var user = (await db.query(mysql.format(getUser, [req.query.password]), {hash: "getUser"}))[0];
   if( user.length === 0 ){
     res.status(404).send(`User not found`);
     return;
   }
 
-  const getUserSessions = "SELECT id, startTime, endTime FROM sessions WHERE password = BINARY ?";
-  db.query(mysql.format(getUserSessions, [req.query.password]), function (error, response) {
-    if (error) {
+  db.query(mysql.format(getUserSessions, [req.query.password]), {hash: "getUserSessions"})
+    .then(response => {
+      var formatted = [];
+      response[0].forEach( session => {
+        formatted.push(
+          {
+            "id": session['id'],
+            "startTime": session['startTime'],
+            "endTime": session['endTime']
+          }
+        )
+      });
+       res.json(formatted);
+    })
+    .catch(error => {
       console.log(error);
       res.status(500).send('Something went wrong');
-    }
-    var formatted = [];
-    response.forEach( session => {
-      formatted.push(
-        {
-          "id": session['id'],
-          "startTime": session['startTime'],
-          "endTime": session['endTime']
-        }
-      )
-    });
-    res.json(formatted);
-  });
+    })
 });
 
 router.get('/getuserdata', async (req, res, next) => {
-  const getUser = "SELECT name, signedIn, lastTime FROM users WHERE password = BINARY ?";
-  var user = await db.awaitQuery(mysql.format(getUser, [req.query.password]));
+  var user = (await db.query(mysql.format(getUser, [req.query.password]), {hash: "getUser"}))[0];
+
   if( user.length === 0 ){
     res.status(404).send(`User not found`);
     return;
   }
 
-  const getUserSessions = "SELECT startTime, endTime FROM sessions WHERE password = BINARY ?";
-  db.query(mysql.format(getUserSessions, [req.query.password]), function (error, response) {
-    if (error) {
+  db.query(mysql.format(getUserSessions, [req.query.password]), {hash: "getUserSessions"})
+    .then(response => {
+      var totalTime = 0;
+      var meetings = 0;
+      for( const session of response[0] ){
+        if(session['endTime'] - session['startTime'] < parseInt(process.env.MAX_TIME)){
+          totalTime += session['endTime'] - session['startTime'];
+          if(new Date(new Date(session['startTime']).toLocaleString("en-US", {timeZone: "America/Los_Angeles"})).getDay() === parseInt(process.env.MEETING_DAY))
+            meetings ++;
+        }
+      }
+      res.json({
+        "name": user[0]['name'],
+        "signedIn": user[0]['signedIn'],
+        "totalTime": totalTime,
+        "meetings": meetings
+      });
+    })
+    .catch(error => {
       console.log(error);
       res.status(500).send('Something went wrong');
-    }
-
-    var totalTime = 0;
-    var meetings = 0;
-    for( const session of response ){
-      if(session['endTime'] - session['startTime'] < parseInt(process.env.MAX_TIME)){
-        totalTime += session['endTime'] - session['startTime'];
-        if(new Date(new Date(session['startTime']).toLocaleString("en-US", {timeZone: "America/Los_Angeles"})).getDay() === parseInt(process.env.MEETING_DAY))
-          meetings ++;
-      }
-    }
-
-    res.json({
-      "name": user[0]['name'],
-      "signedIn": user[0]['signedIn'],
-      "totalTime": totalTime,
-      "meetings": meetings
-    });
-  });
+      return;
+    })
 });
 
 router.get('/getusers', async (req, res, next) => {
-  const getUsers = "SELECT id, name, password, signedIn, lastTime FROM users";
-  const getSessions = "SELECT password, startTime, endTime FROM sessions";
   var users = [];
   var userData = {};
 
-  var sessions = await db.awaitQuery(getSessions);
+  var sessions = (await db.query(getSessions, {hash: "getSessions"}))[0];
   for( const session of sessions ){
     if(session['endTime'] - session['startTime'] < parseInt(process.env.MAX_TIME)){
       userData[session['password']] = userData[session['password']] || {'totalTime': 0, 'meetings': 0};
@@ -270,25 +287,24 @@ router.get('/getusers', async (req, res, next) => {
     }
   }
 
-  db.query(mysql.format(getUsers), function (error, response) {
-    if (error) {
+  db.query(getUsers, {hash: "getUsers"})
+    .then(response => {
+      response[0].forEach( user => {
+        users.push({
+          "id": user['id'],
+          "name": user['name'],
+          "signedIn": user['signedIn'],
+          "timeIn": user['signedIn'] === 1 ? Date.now() - user['lastTime'] : 0,
+          "totalTime": userData[user['password']]?.totalTime || 0,
+          "meetings": userData[user['password']]?.meetings || 0,
+        })
+      });
+    res.json(users);
+    })
+    .catch(error => {
       console.log(error);
       res.status(500).send('Something went wrong');
-    }
-
-    response.forEach( user => {
-      users.push({
-        "id": user['id'],
-        "name": user['name'],
-        "signedIn": user['signedIn'],
-        "timeIn": user['signedIn'] === 1 ? Date.now() - user['lastTime'] : 0,
-        "totalTime": userData[user['password']]?.totalTime || 0,
-        "meetings": userData[user['password']]?.meetings || 0,
-      })
-    });
-
-    res.json(users);
-  });
+    })
 });
 
 module.exports = router;
