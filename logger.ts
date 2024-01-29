0e28676443677bd8d104c00e3faba6dde591bf5b
DigitalOcean Droplet <dev@team4159.org>
2024-01-29 14:56:23 -0800

    Fix logging filename

--- logger.ts
+++ logger.ts
@@ -8,10 +8,28 @@ const logger = winston.createLogger({
         winston.format.errors({ stack: true }),
     ),
     transports: [
-        new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-        new winston.transports.File({ filename: "logs/debug.log" }),
+        new winston.transports.File({ filename: newLogFileName("error"), level: "error" }),
+        new winston.transports.File({ filename: newLogFileName("debug") }),
         new winston.transports.Console(),
     ],
 });
+
+function newLogFileName(type: string): string {
+    const pathSegments = [];
+    pathSegments.push("logs");
+    if (process.env.NODE_ENV === "testing") { pathSegments.push("tests") }
+    pathSegments.push(type);
+    pathSegments.push(type + "_" + dateToTimestamp());
+
+    return pathSegments.join("/") + ".log";
+}
+
+function dateToTimestamp(date?: Date): string {
+    if (!date) {
+        date = new Date();
+    }
+
+    return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + "-" + date.getTime();
+}
 
 export default logger;
